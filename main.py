c8dd5845a962bb97442aba216cd5135f723d9f40
John Torres <jandrewtorres5@gmail.com>
2021-05-24 15:30:54 -0700

    added spacy

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("CalPAss")
+import configparser
+import pymysql.cursors
+
+# extract db credentials from config file "db.ini"
+config = configparser.ConfigParser()
+config.read('db.ini')
+
+host = config['mysql']['host']
+user = config['mysql']['user']
+passwd = config['mysql']['passwd']
+db = config['mysql']['db']
+
+# Connect to the database
+connection = pymysql.connect(host,
+                             user,
+                             password,
+                             database,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+with connection:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        print(result)
