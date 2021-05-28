ecf81b2772063bae5e46a1241b231b213173c1ad
hborlik <hborlik@users.noreply.github.com>
2021-05-28 10:24:56 -0700

    updated regex

--- normal_qustion_parser.py
+++ normal_qustion_parser.py
@@ -3,14 +3,15 @@ import pathlib
 import re
 import json
 
-def parse_file(file, var_map=None):
-    question_answer_pattern = re.compile(r"^[\w\s]*\|\s*([^?|.]*)(?:[?\s|.]*)\s*(.*)")
+def parse_file(file, var_map=None, replace=None):
+    question_answer_pattern = re.compile(r"^[\w\s]*\|\s*([^?|]*)(?:[?\s|.]*)\s*(.*)")
     variable_pattern = re.compile(r"\[([^\[\]]*)\]")
 
     day_pattern = re.compile(r"(monday|tuesday|wednesday|thursday|friday|saturday|sunday)", re.IGNORECASE)
 
     variables = set()
     questions = []
+    replace = {} if replace is None else replace
 
     if file.exists() and file.is_file():
         for line in file.open().readlines():
@@ -21,7 +22,13 @@ def parse_file(file, var_map=None):
                 groups = question_answer.groups()
                 question = groups[0]
                 answer = groups[1]
+
+                for r,s in replace.items():
+                    question = re.sub(r, s, question)
+                    answer = re.sub(r, s, answer)
+
                 question = re.sub(r"\s+", ' ', question)
+                answer = re.sub(r"\s+", ' ', answer)
 
                 # help out questions with specific data points in them
                 question = day_pattern.sub('[day]', question)
@@ -40,7 +47,7 @@ def parse_file(file, var_map=None):
                     return string, va
                 question, var = var_replacer(question)
                 # answer = var_replacer(answer)
-                print(question)
+                # print(question)
                 # print(f'{groups[0]} -> {question}')
                 questions.append(question)
                 for v in var:
@@ -61,6 +68,7 @@ def main():
 
     parser.add_argument('--dump-vars', dest='vars', action='store_true', required=False, help='output set of var strings in file')
     parser.add_argument('--var-file', type=str, required=False, help='file to load or store variable mappings', metavar='V', dest='varfile')
+    parser.add_argument('--out', type=str, required=False, help='file to store parsed questions', metavar='o', dest='outfile')
 
     args = parser.parse_args()
     # for f in pathlib.Path(args.indir).glob('*.txt'):
@@ -83,7 +91,7 @@ def main():
                 print(f'invalid var_map file {e}')
 
 
-    var, questions = parse_file(file, var_map)
+    var, questions = parse_file(file, var_map["map"], var_map["replace"])
 
     if args.varfile and not varfile.exists():
         if var_map is None:
@@ -96,6 +104,19 @@ def main():
 
     if args.vars:
         print(list(var))
+
+    if args.outfile is None:
+        for q in questions:
+            print(q)
+    else:
+        outfile = pathlib.Path(args.outfile)
+        outfile.touch(mode=0o755)
+        if outfile.exists() and outfile.is_file():
+            with outfile.open(mode='w') as wo:
+                wo.writelines([q + '\n' for q in questions])
+        else:
+            print(f'{outfile.absolute()} is not a valid destination')
+            exit(-1)
 
     
 
